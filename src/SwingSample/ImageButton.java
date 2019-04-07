ef2ebc15039eb2ab4440c22a71fb38f8305c1a47
RayLiu <ray6.ray61027@gmail.com>
2019-04-07 19:05:17 +0800

    modified path

--- src/SwingSample/ImageButton.java
+++ src/SwingSample/ImageButton.java
@@ -4,8 +4,9 @@ import javax.swing.*;
 public class ImageButton {
     ImageButton(){
         JFrame f = new JFrame("Image button");
-        JButton b = new JButton(new ImageIcon("C:\\Users\\ray\\Desktop\\save.jpg"));
-        b.setBounds(60, 60, 188, 188);
+        JButton b = new JButton(new ImageIcon("/Users/RayLiu/IdeaProjects/OOOO/img/rabbit.png"));
+
+        b.setBounds(60, 60, 50, 50);
         f.add(b);
         f.setSize(300, 400);
         f.setLayout(null);
