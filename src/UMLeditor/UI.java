7b08e2735c8524f200270c228eceacefe8c4ad7e
ray6 <ray6.ray61027@gmail.com>
2019-04-11 21:26:07 +0800

    Placed Basic Component

--- src/UMLeditor/UI.java
+++ src/UMLeditor/UI.java
@@ -1,55 +1,40 @@
 package UMLeditor;
+
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.util.HashSet;
-import java.util.Set;
+import javax.swing.*;
 
-import javax.swing.Icon;
-import javax.swing.ImageIcon;
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JMenu;
-import javax.swing.JMenuBar;
-import javax.swing.JMenuItem;
-import javax.swing.JOptionPane;
-
-public class UI extends  JFrame{
-    private Canvas mycanvas = new Canvas();
-    private JMenuBar jmb;
-    private JMenu file = new JMenu("File");
-    private JMenu edit = new JMenu("Edit");
-    private JMenuItem [] file_item = new JMenuItem[3];//選單1
-    private JMenuItem [] edit_item = new JMenuItem[3];//選單2
-    private JButton[] btns = new JButton[6];
+public class UI extends JFrame {
+    private Canvas canvas;
+    private  JMenuBar jmb;
+    private JMenu file;
+    private  JMenu edit;
+    private JMenuItem [] file_item = new JMenuItem[2];
+    private  JMenuItem [] edit_item = new JMenuItem[3];
+    private  JButton [] buttons = new JButton[6];
 
     public UI(){
-        init();
+        setSize(1000, 600);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         initMenu();
         initButton();
+        addComponentToPane(this.getContentPane());
 
+        setResizable(true);
+        pack();
+        setVisible(true);
     }
-    private void init() {
-        // TODO Auto-generated method stub
-        this.setLayout(null);
-        this.add(mycanvas);
-        this.setSize(1000,600);
-        this.setLocation(50,10);
-        this.setResizable(false);//視窗放大按鈕無效
-        this.setVisible(true);
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    }
-    private void initMenu() {
-        // TODO Auto-generated method stub
+    private void initMenu(){
         jmb = new JMenuBar();
-        this.setJMenuBar(jmb); //加入工具列
-        //遊戲的選擇項目
+        setJMenuBar(jmb);
+
+        file = new JMenu("File");
         jmb.add(file);
         file_item[0] = new JMenuItem("1");
         file_item[1] = new JMenuItem("2");
         file.add(file_item[0]);
         file.add(file_item[1]);
-        //關於的選擇項目
+
+        edit = new JMenu("Edit");
         jmb.add(edit);
         edit_item[0] = new JMenuItem("change object name");
         edit_item[1] = new JMenuItem("group");
@@ -57,22 +42,31 @@ public class UI extends  JFrame{
         edit.add(edit_item[0]);
         edit.add(edit_item[1]);
         edit.add(edit_item[2]);
-
     }
-    private void initButton() {
-        // TODO Auto-generated method stub
-        int totaly=5;
-        for(int i=0;i<=5;i++)
-        {
-            btns[i]=new JButton();
-            Icon imgicon = new ImageIcon("res/"+String.valueOf(i+1)+".png");
-            btns[i].setIcon(imgicon);
-            this.add(btns[i]);
-            btns[i].setBounds(10, totaly, 50, 50);
-            totaly=totaly+65;
 
+    private void initButton(){
+        String img_path = "C:\\Users\\ray\\IdeaProjects\\OOOO\\img\\";
+        String [] img = {"arrow.png", "Line.png", "gen.png", "comp.png", "class.png", "use.png"};
+        for(int i =0; i<6; i++){
+            buttons[i] = new JButton(new ImageIcon(img_path+img[i]));
+            buttons[i].setBackground(Color.white);
         }
     }
+    public void addComponentToPane(Container pane){
+
+        pane.setLayout(new BorderLayout());
+        JPanel p = new JPanel();
+        p.setLayout(new GridLayout(6, 1));
+
+        for(int i =0; i<6 ; i++){
+            p.add(buttons[i]);
+        }
+        add(p, BorderLayout.WEST);
+        p = new MyCanvas();
+        add(p, BorderLayout.CENTER);
+
+    }
+
 
     public static void main(String[] args) {
         new UI();
