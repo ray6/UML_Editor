e9a2fb60ee0305f8707d325da723fa8bb02dac1e
ray6 <ray6.ray61027@gmail.com>
2019-06-21 00:00:51 +0800

    add Button Class

---
+++ src/Button/GenerationLineButton.java
@@ -0,0 +1,26 @@
+package Button;
+
+import UMLeditor.MyCanvas;
+import UMLeditor.UI;
+
+import javax.swing.*;
+import javax.swing.border.Border;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class GenerationLineButton extends JButton implements ActionListener {
+    int no;
+    public GenerationLineButton(Icon icon, int number){
+        super(icon);
+        no = number;
+        Border emborder = BorderFactory.createEmptyBorder();
+        setBackground(Color.white);
+        setBorder(emborder);
+        addActionListener(this);
+    }
+    public void actionPerformed(ActionEvent e){
+        UI.getInstance().switchIcon(no);
+        MyCanvas.getInstance().ModeSwitcher(no);
+    }
+}
