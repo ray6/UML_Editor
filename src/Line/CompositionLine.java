f0a7590c4f2a39214485c92673ecc6db9ee2ec37
ray6 <ray6.ray61027@gmail.com>
2019-06-21 00:21:24 +0800

    remove class polygonLine GenLine and ComLine inherit from Line

--- src/Line/CompositionLine.java
+++ src/Line/CompositionLine.java
@@ -4,11 +4,19 @@ import BasicObject.Port;
 
 import java.awt.*;
 
-public class CompositionLine extends PolygonLine {
-
+public class CompositionLine extends Line {
+    protected int corner_num;
+    protected int[][] points;
+    //points[0] : x coordinate
+    //points[1] : y coordinate
     public CompositionLine(Port start, Port end){
         super(start, end);
         corner_num = 4;
     }
-
+    public void draw(Graphics g) {
+        g.setColor(Color.black);
+        g.drawLine(Start.getX(), Start.getY(), End.getX(), End.getY());
+        points = getRegularPolygon(Start.getX(), Start.getY(), End.getX(), End.getY(), corner_num);
+        g.fillPolygon(points[0], points[1], corner_num);
+    }
 }
--- src/Line/GeneralizationLine.java
+++ src/Line/GeneralizationLine.java
@@ -4,12 +4,20 @@ import BasicObject.Port;
 
 import java.awt.*;
 
-import static java.lang.Math.cos;
 
-public class GeneralizationLine extends PolygonLine {
+public class GeneralizationLine extends Line {
+    protected int corner_num;
+    protected int[][] points;
+    //points[0] : x coordinate
+    //points[1] : y coordinate
     public GeneralizationLine(Port start, Port end){
         super(start, end);
         corner_num = 3;
     }
-
+    public void draw(Graphics g) {
+        g.setColor(Color.black);
+        g.drawLine(Start.getX(), Start.getY(), End.getX(), End.getY());
+        points = getRegularPolygon(Start.getX(), Start.getY(), End.getX(), End.getY(), corner_num);
+        g.fillPolygon(points[0], points[1], corner_num);
+    }
 }
